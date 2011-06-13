6caf30b6795d441fd4ff7b56a50501b32d70ef31
Mrityunjay Ravi Iyer <develmj@gmail.com>
2011-06-13 21:48:55 +0530

    Added monitor code: new threaded inotify system to inform of new files or changes

--- monitor.rb
+++ monitor.rb
@@ -2,22 +2,63 @@
 
 require 'rubygems'
 require 'rb-inotify'
+require 'pp'
 
-WDIR = "/home/mj/torture/temp"
+WDIR = "/tmp"
+START_TIME = Time.now
 
-def create_notify(event)
-  puts "create notify for #{event.name}"
+
+def process_hash(a)
+  a.delete_if{|x,y| y[:flags].include?(:delete) }#or y[:flags].include?(:moved_to) }
 end
 
-def close_notify(event)
-  puts "close notify for #{event.name}"
+def commit_write(hash)
+  pp hash.to_a.select{|file|
+    file[1][:mtime] > file[1][:old_mtime]
+  }
+  puts "--"
+  pp H
+  puts "----------------"
 end
 
-create_notifier = INotify::Notifier.new
-create_notifier.watch(WDIR,:create){|x| create_notify(x)}
+H = {}
 
-close_notifier = INotify::Notifier.new
-close_notifier.watch(WDIR,:close){|x| close_notify(x)}
+monitor_thread = Thread.new {
 
-create_notifier.run
-close_notifier.run
+  notifier = INotify::Notifier.new
+  notifier.watch(WDIR,:all_events){|x|
+    begin
+      unless x.flags.include?(:isdir)
+        if File.exist?("#{WDIR}/#{x.name}")
+          old_mtime = 0
+          if H[x.name]
+            old_mtime = H[x.name][:mtime]
+          end
+          H[x.name] = {:flags => x.flags, :mtime => File.mtime("#{WDIR}/#{x.name}").to_i, :old_mtime => old_mtime }
+        else
+          H.delete(x.name)
+        end
+      end
+    rescue 
+    end
+  }
+
+  while true
+    if IO.select([notifier.to_io], [], [], 0.0001)
+      notifier.process
+    end
+    process_hash(H)
+  end
+}
+
+printer_thread = Thread.new {
+  while true
+    sleep (3)
+    process_hash(H)
+    commit_write(H)
+  end
+}
+
+monitor_thread.join
+printer_thread.join
+
