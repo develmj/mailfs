9587b85fcabda0f935b08913404078dcc997d3ac
Mrityunjay Ravi Iyer <develmj@gmail.com>
2011-06-13 23:20:49 +0530

    stuck: mutex causing issues

--- monitor.rb
+++ monitor.rb
@@ -3,25 +3,36 @@
 require 'rubygems'
 require 'rb-inotify'
 require 'pp'
+require 'thread'
 
 WDIR = "/home/mj/torture/temp"
 START_TIME = Time.now
-
+H = {}
+semaphore = Mutex.new
+cv = ConditionVariable.new
 
 def process_hash(a)
   a.delete_if{|x,y| y[:flags].include?(:delete) }#or y[:flags].include?(:moved_to) }
 end
 
 def commit_write(hash)
-  pp hash.to_a.select{|file|
+  selected = hash.to_a.select {|file|
     file[1][:mtime] > file[1][:old_mtime]
   }
+  if selected.length > 0
+    semaphore.synchronize {
+      cv.wait(semaphore)  
+      selected.each {|x|
+        puts "Processed: #{x[0]}"
+        hash.delete(x[0])
+      }
+    }
+  end
   puts "--"
   pp H
   puts "----------------"
 end
 
-H = {}
 
 monitor_thread = Thread.new {
 
@@ -33,9 +44,15 @@ monitor_thread = Thread.new {
           old_mtime = 0
           mtime = File.mtime("#{WDIR}/#{x.name}").to_i
           old_mtime = H[x.name][:mtime] if H[x.name] and H[x.name][:mtime] < mtime
-          H[x.name] = {:flags => x.flags, :mtime => mtime, :old_mtime => old_mtime }
+          semaphore.synchronize {
+            cv.wait(semaphore)
+            H[x.name] = {:flags => x.flags, :mtime => mtime, :old_mtime => old_mtime}
+          }
         else
-          H.delete(x.name)
+          semaphore.synchronize {
+            cv.wait(semaphore)
+            H.delete(x.name)
+          }
         end
       end
     rescue
@@ -46,7 +63,10 @@ monitor_thread = Thread.new {
     if IO.select([notifier.to_io], [], [], 0.0001)
       notifier.process
     end
-    process_hash(H)
+    semaphore.synchronize {
+      cv.wait(semaphore)
+      process_hash(H)
+    }
   end
 }
 
